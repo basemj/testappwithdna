3b9a2a6321771449bd7ba2b24bc7b1a98ae14b08
BasemJ <basem.jawahri@justgiving.com>
2015-08-07 13:43:03 +0100

    initial commit

---
+++ TestAppWithDNA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAppWithDNA.Startup))]
+namespace TestAppWithDNA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
