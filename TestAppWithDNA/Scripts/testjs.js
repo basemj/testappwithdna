ae97260703d77e4b7c3fc330de224b9f4277e2c2
BasemJ <basem.jawahri@justgiving.com>
2015-08-10 11:38:59 +0100

    added dev dependencies and included clear-input module

---
+++ TestAppWithDNA/Scripts/testjs.js
@@ -0,0 +1,12 @@
+﻿var ClearInput = require('../bower_components/dna-form/components/js/clear-input');
+
+// Use the module however you need....
+var inputs = document.querySelectorAll('[data-clearinput]');
+[].slice.call(inputs).forEach(function (input) {
+    if (input.getAttribute('data-clearinput') === 'false') {
+        return;
+    }
+    new ClearInput({
+        el: input
+    });
+});
---
+++ TestAppWithDNA/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/// <binding ProjectOpened='LessAndMinifyCSS, bundleJavaScript' />
+"use strict";
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var browserify = require('gulp-browserify');
+
+gulp.task('LessAndMinifyCSS', function () {
+    gulp.src('Content/testmain.less')
+        .pipe(less())
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('bundleJavaScript', function () {
+    gulp.src('Scripts/testjs.js')
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: !gulp.env.production
+        }))
+        .pipe(gulp.dest('wwwroot/js'));
+});
